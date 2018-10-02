0f0b8870c7686ef5c39042ab7bc1dce774fcaa76
Michael Warf <devbox@gmail.com>
2018-10-02 15:40:40 -0600

    Edited files on linux laptop

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
+    <div className="Hero">
+      <div className="HeroGroup">
     <h1>Learn to <br/>design and code React apps</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
 
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/video">Watch the video</Link>
+    </div>
+  </div>
   </div>
 )
 
